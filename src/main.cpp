c81fac23e3307d3ba371e31e2b3e2d9c02b4537e
vijayrao1988 <vijayrao1988@gmail.com>
2019-05-22 11:56:11 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  Serial.println("Sample serial data");
+  delay(1000);
+  digitalWrite(13, LOW);
+  Serial.println("Sample serial data");
+  delay(1000);
+}
